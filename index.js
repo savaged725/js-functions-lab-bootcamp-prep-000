f37b5d5056ac2f1344ddcc679597fc8af45f2868
Deborah Savage <savaged725@yahoo.com>
2019-06-28 15:36:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,3 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${Name}!`
 }
 
-function holidayCountdown(holiday, days) {
-  return 'It\'s ${days} days until ${holiday}!' 
-}
